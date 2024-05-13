90e03df8a06fbac034730e62510e83f091226112
AsangaDeSilva <asanganavodidesilva@gmail.com>
2024-05-13 22:45:11 +0530

    added some files

--- app/src/main/java/com/example/reminderapp/MainActivity.kt
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.reminderapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues
+import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.reminderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NoteDatabaseHelper
+    private lateinit var notesAdopter: NotesAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+        notesAdopter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdopter
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notesAdopter.refreshData(db.getAllNotes())
     }
 }
